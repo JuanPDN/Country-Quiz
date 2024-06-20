cef1ad90e20c0106d0abfd4841c167eee8cc5c0c
Juan Pablo Delgado <jpdelgado9641@gmail.com>
2024-06-20 17:48:47 -0500

    add navigation bar

---
+++ src/app/navbar/navbar.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-navbar',
+  standalone: true,
+  imports: [],
+  templateUrl: './navbar.component.html',
+  styleUrl: './navbar.component.css'
+})
+export class NavbarComponent {
+  numbers = Array.from({ length: 10 }, (_, i) => i + 1);
+}
