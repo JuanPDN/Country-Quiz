de0b9132b6f884ae9ba2ecede3657b8fba60b57a
Juan Pablo Delgado <jpdelgado9641@gmail.com>
2024-06-21 17:38:55 -0500

    design finish game page

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,11 +2,12 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { NavbarComponent } from './navbar/navbar.component';
 import { GameComponent } from './game/game.component';
+import { FinishComponent } from './finish/finish.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, NavbarComponent, GameComponent],
+  imports: [RouterOutlet, NavbarComponent, GameComponent, FinishComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
---
+++ src/app/finish/finish.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import { NgOptimizedImage } from '@angular/common';
+
+@Component({
+  selector: 'app-finish',
+  standalone: true,
+  imports: [NgOptimizedImage],
+  templateUrl: './finish.component.html',
+  styleUrl: './finish.component.css'
+})
+export class FinishComponent {
+
+}
