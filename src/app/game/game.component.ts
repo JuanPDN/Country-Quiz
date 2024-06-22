f65bf3131a93b17cd534adab479c0b4f0e105183
Juan Pablo Delgado <jpdelgado9641@gmail.com>
2024-06-22 11:23:20 -0500

    Routing

--- src/app/game/game.component.ts
+++ src/app/game/game.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
+import { NavbarComponent } from "../navbar/navbar.component";
 
 @Component({
-  selector: 'app-game',
-  standalone: true,
-  imports: [],
-  templateUrl: './game.component.html',
-  styleUrl: './game.component.css'
+    selector: 'app-game',
+    standalone: true,
+    templateUrl: './game.component.html',
+    styleUrl: './game.component.css',
+    imports: [NavbarComponent]
 })
 export class GameComponent {
 
