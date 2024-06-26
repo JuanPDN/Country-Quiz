75bca8bc4709f6c63390334f24a7adf0d31db696
Juan Pablo Delgado <jpdelgado9641@gmail.com>
2024-06-26 14:52:57 -0500

    add When users select an answer, they should get the correct answer immediately with according indicators in the design

--- src/app/game/game.component.ts
+++ src/app/game/game.component.ts
@@ -1,8 +1,9 @@
 import { Component, OnInit, inject } from '@angular/core';
-import { ActivatedRoute } from '@angular/router';
+import { ActivatedRoute, Router } from '@angular/router';
 
 import { NavbarComponent } from "../navbar/navbar.component";
 import { GetDataService } from '../services/get-data.service';
+
 
 
 @Component({
@@ -15,32 +16,34 @@ import { GetDataService } from '../services/get-data.service';
 export class GameComponent implements OnInit {
     route: ActivatedRoute = inject(ActivatedRoute);
     questionId: number = 1;
-    response: boolean = false;
     questions: any[] = [];
 
-    constructor(private dataService: GetDataService) {
-        this.route.params.subscribe(params => {
-            this.questionId = Number(params['questionId']);
-        });
+    constructor(private dataService: GetDataService, private router: Router) {
+        this.route.params.subscribe(params =>
+            this.questionId = Number(params['questionId'])
+        );
     }
 
-    seeOptions(event: Event) {
+    submitAnswer(event: Event) {
         let response = (event.target as HTMLButtonElement).textContent
         if (!this.questions[this.questionId - 1].response) {
-            this.questions[this.questionId - 1].yourAnswer = response
-            this.questions[this.questionId - 1].response = true
+            this.questions[this.questionId - 1].yourAnswer = response?.trim();
+            this.questions[this.questionId - 1].response = true;
+            this.nextQuestion()
         }
-        console.log(this.questions);
-
     }
 
-    ngOnInit(): void {
-        this.dataService.getAllData().subscribe(
-            (data) => {
-                this.questions = data;
-
+    nextQuestion() {
+        setTimeout(() => {
+            if (this.questionId < 10) {
+                this.router.navigate(['/game', this.questionId + 1])
             }
-        )
+        }, 1500)
+    }
 
+
+    ngOnInit(): void {
+        this.dataService.getAllData().subscribe((data) =>
+            this.questions = data)
     }
 }
