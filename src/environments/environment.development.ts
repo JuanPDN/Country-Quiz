20169b207056bce9728d18b23e9885f98efbed3f
Juan Pablo Delgado <jpdelgado9641@gmail.com>
2024-06-28 10:44:54 -0500

    configure environments

--- src/environments/environment.development.ts
+++ src/environments/environment.development.ts
@@ -1,4 +1,3 @@
 export const environment = {
-    production: false,
     API_URL: 'https://country-page-jp.vercel.app/api/countries',
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,4 +1,3 @@
 export const environment = {
-   production: true,
    API_URL: 'https://restcountries.com/v3.1/all',
 };
