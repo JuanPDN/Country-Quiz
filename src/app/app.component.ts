0f56e5e28e1908656e22f732d460f6b5c2153316
Juan Pablo Delgado <jpdelgado9641@gmail.com>
2024-06-28 14:07:14 -0500

    clear console log and unused imports

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,11 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import { NavbarComponent } from './navbar/navbar.component';
-import { GameComponent } from './game/game.component';
-import { FinishComponent } from './finish/finish.component';
+
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, NavbarComponent, GameComponent, FinishComponent],
+  imports: [RouterOutlet],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
