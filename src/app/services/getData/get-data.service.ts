9937ca010d0ad0ff679865d0d28a8e36825d38de
Juan Pablo Delgado <jpdelgado9641@gmail.com>
2024-06-28 10:04:44 -0500

    add enviroments

--- src/app/services/getData/get-data.service.ts
+++ src/app/services/getData/get-data.service.ts
@@ -1,17 +1,25 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
-import { Observable, map } from 'rxjs';
+import { Observable, map, retry } from 'rxjs';
 import { generateQuestions } from '../../utils/questionsGenerator';
+import { environment } from '../../../environments/environment';
+
+
 
 @Injectable({
   providedIn: 'root'
 })
 export class GetDataService {
 
+
   constructor(private http: HttpClient) { }
 
   getAllData(): Observable<any> {
-    return this.http.get('https://country-page-jp.vercel.app/api/countries').pipe(map((data: any) =>
+    return this.http
+    .get(environment.API_URL)
+    .pipe(
+      retry(1),
+      map((data: any) =>
       generateQuestions(data)
     ));
   }
---
+++ src/environments/environment.development.ts
@@ -0,0 +1,4 @@
+export const environment = {
+    production: false,
+    API_URL: 'https://country-page-jp.vercel.app/api/countries',
+};
